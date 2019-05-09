2b0128dc692019436dbdeb8243e6d460ca0162b9
Spencer K-W <keashlywarters@gmail.com>
2019-05-09 14:48:07 -0400

    added some comments for explanation

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
 "use strict";
 
+//holds each item the user adds (name and cost)
 class BudgetItem {
   constructor(name, cost) {
     this.name = name;
     this.cost = cost;
   }
+  //returns a formatted HTML block for the item's li element
   displayHTML() {
     return `
     <p>${this.name}</p>
@@ -14,17 +16,22 @@ class BudgetItem {
   }
 }
 
+//represents each category in the budget (bills, food, clothing, entertainment)
+//each category holds its own items
 class BudgetCategory {
   constructor(name) {
     this.name = name;
     this.items = [];
   }
+  //add an item with the given values to the array
   addItem(name, cost) {
     this.items = [...this.items, new BudgetItem(name, cost)];
   }
+  //remove an item at the given index from the array
   removeItemAt(index) {
     this.items = [...this.items.slice(0, index), ...this.items.slice(index + 1)];
   }
+  //return the total cost of this category's items
   calculateTotal() {
     let total = 0;
     for (let item of this.items) {
@@ -32,6 +39,8 @@ class BudgetCategory {
     }
     return total;
   }
+  //update the category's DOM display
+  //includes the list of items and the category total cost
   display() {
     let categoryUL = document.querySelector(`ul.item-list.${this.name}`);
     categoryUL.innerHTML = "";
@@ -49,6 +58,8 @@ class BudgetCategory {
   }
 }
 
+//contains all the budget info for the site
+//directly contains the 4 categories, and indirectly their items
 class TotalBudget {
   constructor() {
     this.maxBudget = 0;
@@ -59,17 +70,24 @@ class TotalBudget {
       new BudgetCategory("entertainment")
     ];
   }
+  //returns the category with the given name
+  //useful for in and outside of this object
   getCategory(categoryName) {
     return this.categories.find(category => category.name === categoryName);
   }
+  //tells the given category to add an item with the given values to itself
+  //also updates the total budget display
   addItem(name, cost, categoryName) {
     this.getCategory(categoryName).addItem(name, cost);
     this.displayBudgetRemaining();
   }
+  //tells the given category to remove its item at the given index
+  //also updates the total budget display
   removeItem(index, categoryName) {
     this.getCategory(categoryName).removeItemAt(index);
     this.displayBudgetRemaining();
   }
+  //returns the total cost of the budget (from all categories)
   calculateTotal() {
     let total = 0;
     for (let category of this.categories) {
@@ -77,9 +95,11 @@ class TotalBudget {
     }
     return total;
   }
+  //tells the given category to update its display
   displayCategory(categoryName) {
     this.getCategory(categoryName).display();
   }
+  //updates the display for the remaining budget
   displayBudgetRemaining() {
     let remaining = this.maxBudget - this.calculateTotal();
     document.querySelector("p.budget-remaining").innerText = `Remaining Budget: $${remaining}`;
@@ -87,9 +107,13 @@ class TotalBudget {
   }
 }
 
+//the budget object
 const totalBudget = new TotalBudget();
 //console.log(totalBudget.addItem);
 
+//handler for when one of the add item forms is submitted
+//adds the item to that category, updates that category's display
+//resets the form
 function addItemToCategory(event) {
   event.preventDefault();
   // console.dir(event);
@@ -111,6 +135,8 @@ function addItemToCategory(event) {
   event.target.children[1].value = "";
 }
 
+//handler for when one of the items is removed
+//removes the item from the appropriate category and redisplays that category
 function removeItemFromCategory(event) {
   //get the index attribute from the list item
   let index = event.target.parentNode.attributes["index"].value;
@@ -120,6 +146,7 @@ function removeItemFromCategory(event) {
   totalBudget.displayCategory(categoryName);
 }
 
+//handler for when the user updates their max budget
 function updateMaxBudget(event) {
   event.preventDefault();
   totalBudget.maxBudget = Number(event.target.children[0].value);
@@ -129,6 +156,7 @@ function updateMaxBudget(event) {
   event.target.children[0].value = ""; //clear the input
 }
 
+//add event listeners to the main for both clicking buttons and submitting forms
 let main = document.querySelector("main");
 main.addEventListener("submit", function(event) {
   if (event.target.classList.contains("add-item")) {
@@ -137,7 +165,6 @@ main.addEventListener("submit", function(event) {
     updateMaxBudget(event);
   }
 });
-
 main.addEventListener("click", function(event) {
   if (event.target.classList.contains("remove-btn")) {
     removeItemFromCategory(event);
